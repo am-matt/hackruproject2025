b4857ced01e7c47312977d8366ac535a9d43c67c
Matthew Miller <matthew.am910@gmail.com>
2025-10-05 02:33:00 -0400

    fix: discover page break

--- src/pages/Discovery.jsx
+++ src/pages/Discovery.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Nav from '../components/Nav'
+import Event from '../components/Event'
 import { supabase } from '../supabaseClient';
 
 export default function Discovery() {
@@ -76,10 +77,23 @@ export default function Discovery() {
     return (
         <div>
             <Nav />
-            <h1 className="text-center text-white font-roboto text-2xl mt-3 font-semibold">All Events</h1>
+            <h1 class="text-center text-white font-roboto text-2xl mt-3 font-semibold">All Events</h1>
             <br />
-            {eventsWithNames.map(event => (
-                <div key={event.id}>
+            <div class="flex flex-wrap justify-center">
+                {eventsWithNames.map((event) => {
+                    return <Event key={event.id} event={event} session={session} onInterest={markInterested} />
+                })}
+            </div>
+            
+            
+        </div>  
+    )
+    
+}
+
+
+
+/*<div key={event.id}>
                     <h2>Title: {event.title}</h2>
                     <p>Description: {event.description}</p>
                     <p>Location: {event.location}</p>
@@ -91,9 +105,4 @@ export default function Discovery() {
                     </button>
                     <br />
                     <br />
-                </div>
-            ))}
-        </div>  
-    )
-    
-}
+                </div>*/
