a229abc959849fefb37245a7ec82fa727760ede3
Matthew Miller <matthew.am910@gmail.com>
2025-10-05 05:40:48 -0400

    feat: complete create event screen

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,8 +1,9 @@
-import { use, useState } from "react";
+import { use, useState, useRef } from "react";
 import { supabase } from "../supabaseClient";
 import Nav from '../components/Nav'
 
 export default function CreateEvent(){
+    const descriptionTextArea = useRef(null);
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -48,6 +49,7 @@ export default function CreateEvent(){
         setDescription("");
         setWindowSize("");
         setLocation("")
+        descriptionTextArea.current.value = "";
 
     }
 
@@ -55,50 +57,66 @@ export default function CreateEvent(){
     return(
         <>
             <Nav />
-            <h1 style={{textAlign: "center", color: "white", fontSize: "45px"}}>Create an Event</h1>
+            <h1 class="text-white text-2xl text-center mb-4 mt-4">Create an Event</h1>
 
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label> Title:
-                        <input type="text" 
-                            value={title} 
-                            onChange={(e) => setTitle(e.target.value)}
+            <form class="text-center" onSubmit={handleSubmit}>
+                <table class="m-auto  text-left">
+                <tr>
+                    <td>
+                        <label class="text-white text-base font-roboto text-semibold">Title</label>
+                    </td>
+                    <td>
+                        <input class="transition-all mb-1 w-75 h-10 text-semibold font-roboto text-darker-gray ml-5 rounded-lg focus:inset-shadow-innerbox" type="text" 
+                                value={title} 
+                                onChange={(e) => setTitle(e.target.value)}
                         />
-                    </label>
-                </div>
+                        </td>
+                </tr>
                 
-                 <div>
-                    <label> Description:
-                        <input type="text" 
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        
-                        />
-                    </label>
-                </div>
+                 <tr>
+                    <td>
+                        <label class="align-top text-white text-base font-roboto text-semibold">Description</label>
+                    </td>
+                    <td class="flex">
+                        <textarea ref={descriptionTextArea} name="Description" onChange={
+                            (e)=>
+                                
+                                setDescription(e.target.value)
+                            } class="mb-1 transition-all text-darker-gray ml-5 w-75 h-20 text-sm rounded-lg focus:inset-shadow-innerbox"></textarea>
+                    </td>
+                </tr>
 
-                 <div>
-                    <label> Time Needed in Minutes:
-                        <input type="number" 
-                            value={windowSize}
-                            onChange={(e) => setWindowSize(parseInt(e.target.value) )}
-                            />
-                    </label>
-                </div>
-
-                <div>
-                    <label> Location:
-                        <input type="text" 
+                <tr>
+                    <td>
+                        <label class="text-white text-base font-roboto text-semibold">Location</label>
+                    </td>
+                    <td>
+                        <input class="mb-1 h-10 transition-all text-darker-gray w-75 ml-5 rounded-lg focus:inset-shadow-innerbox" type="text" 
                             value={location}
                             onChange={(e) => setLocation((e.target.value))}
                             />
-                    </label>
-                </div>
+                    </td>
+                </tr>
 
-                <button type="submit"> Submit </button>
+                 <tr>
+                    <td>
+                        <label class="text-white text-base font-roboto text-semibold">Timeframe (in minutes)</label>
+                    </td>
+                    <td>
+                        <input class="mb-1 w-30 h-10 transition-all text-semibold font-roboto text-darker-gray ml-5 rounded-lg focus:inset-shadow-innerbox" type="number" 
+                            value={windowSize}
+                            onChange={(e) => setWindowSize(parseInt(e.target.value) )}
+                            />
+                    </td>
+                </tr>
+
+                
+                </table>
+
+                <button type="submit" class="select-none font-bold transition-all mt-2 bg-transparent hover:bg-white text-white hover:text-dark-gray border-2 border-white rounded-lg border-solid p-2 w-40 cursor-pointer hover:inset-shadow-innerbox active:inset-shadow-xs"> Submit </button>
             </form>
 
-            <h2>{ message } </h2>
+            <h2 class="text-center text-white mt-1">{ message } </h2>
         </>
     )
 }
